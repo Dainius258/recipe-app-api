f9497a7b8d40fdb836121d9c7ec30e75e975129b
Dainius258 <dainiuss258@gmail.com>
2024-02-12 17:23:49 +0200

    INITIAL COMMIT + basic API

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import userRouter from "./queries.js";
+import bodyParser from "body-parser";
+const { json, urlencoded } = bodyParser;
+
+const app = express();
+const port = 3000;
+
+app.use(json());
+app.use(
+  urlencoded({
+    extended: true,
+  })
+);
+
+app.get("/", (request, response) => {
+  response.json({ info: "Node.js, Express, and Postgres API" });
+});
+
+app.use("/users", userRouter);
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
+});
