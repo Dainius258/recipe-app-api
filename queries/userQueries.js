c641fb87e2a9274118c726c7f7e244052f1bb380
Dainius258 <dainiuss258@gmail.com>
2024-02-15 14:06:53 +0200

    Added put new user route

--- queries/userQueries.js
+++ queries/userQueries.js
@@ -42,33 +42,25 @@ router.get("/:id", (request, response) => {
   );
 });
 
-/*
-const getUserById = (request, response) => {
-  const id = parseInt(request.params.id);
-
-  pool.query("SELECT * FROM users WHERE id = $1", [id], (error, results) => {
-    if (error) {
-      throw error;
-    }
-    response.status(200).json(results.rows);
-  });
-};
-
-const createUser = (request, response) => {
-  const { name, email } = request.body;
-
+router.put("/newuser", (request, response) => {
+  const { username, password, email } = request.body;
   pool.query(
-    "INSERT INTO users (name, email) VALUES ($1, $2)",
-    [name, email],
+    "INSERT INTO users (username, password, email) VALUES ($1, $2, $3)",
+    [username, password, email],
     (error, results) => {
       if (error) {
         throw error;
       }
-      response.status(201).send(`User added with ID: ${results.insertId}`);
+      response.status(201).send(`User added`);
     }
   );
-};
+});
 
+router.put("/updateuser", (request, response) => {
+  const id = parseInt(request.params.id);
+  const { username, password, email } = request.body;
+});
+/*
 const updateUser = (request, response) => {
   const id = parseInt(request.params.id);
   const { name, email } = request.body;
