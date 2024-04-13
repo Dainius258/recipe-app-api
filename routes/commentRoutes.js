ccccb65952cb29f3174da499e42bfd97259e1b50
Dainius258 <dainiuss258@gmail.com>
2024-04-13 14:36:10 +0300

    put and delete routes for comments

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -5,11 +5,46 @@ import knex from "knex";
 const db = knex(knexConfig.development);
 const router = express.Router();
 
+router.put("/updatecomment/:id", async (request, response) => {
+  try {
+    const commentId = request.params.id;
+    const { comment_text } = request.body;
+
+    const existingComment = await db("comments")
+      .where({ id: commentId })
+      .first();
+    if (!existingComment) {
+      return response.status(404).json({ error: "Comment not found" });
+    }
+
+    await db("comments").where({ id: commentId }).update({ comment_text });
+    return response.status(200).send({ message: `Comment updated` });
+  } catch (error) {
+    return response.status(500).json({ error: error.message });
+  }
+});
+
+router.delete("/deletecomment/:id", async (request, response) => {
+  try {
+    const commentId = request.params.id;
+    const existingComment = await db("comments")
+      .where({ id: commentId })
+      .first();
+    if (!existingComment) {
+      return response.status(404).json({ error: "Comment not found" });
+    }
+    await db("comments").delete().where("id", commentId);
+    return response.status(201).send({ message: `Comment deleted` });
+  } catch (error) {
+    return response.status(500).json({ error: error.message });
+  }
+});
+
 router.post("/newcomment", async (request, response) => {
   try {
     const { recipe_id, user_id, comment_text } = request.body;
     await db("comments").insert({ recipe_id, user_id, comment_text });
-    return response.status(201).send(`Comment added`);
+    return response.status(201).send({ message: `Comment added` });
   } catch (error) {
     return response.status(500).json({ error: error.message });
   }
